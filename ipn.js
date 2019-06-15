3253c8826fd17001089dfd7db5e05e90fe71eeeb
Ronald Luitwieler <ronald@orangewise.com>
2019-06-15 11:08:13 +0200

    update deps

--- ipn.js
+++ ipn.js
@@ -1,97 +1,96 @@
-var https = require('https');
-var _ = require('underscore');
+var https = require('https')
+var _ = require('underscore')
 
-var ppIpn = {};
+var ppIpn = {}
 
-ppIpn.SANDBOX_URL = 'ipnpb.sandbox.paypal.com';
-ppIpn.REGULAR_URL = 'ipnpb.paypal.com';
-
+ppIpn.SANDBOX_URL = 'ipnpb.sandbox.paypal.com'
+ppIpn.REGULAR_URL = 'ipnpb.paypal.com'
 
 ppIpn.parseQuery = function (qstr) {
-  var query = {};
-  var a = qstr.substr(1).split('&');
+  var query = {}
+  var a = qstr.substr(1).split('&')
   for (var i = 0; i < a.length; i++) {
-      var b = a[i].split('=');
-      query[b[0]] = b[1] || '';
+    var b = a[i].split('=')
+    query[b[0]] = b[1] || ''
   }
-  return query;
-};
+  return query
+}
 
 // Build a querystring from the params object.
 ppIpn.stringify = function (params) {
-  var str = '';
-  var pairs = _.pairs(params);
+  var str = ''
+  var pairs = _.pairs(params)
   _.each(pairs, function eachPair (pair) {
-    if (pair[1].indexOf('%') > -1) // skip
-    {
-      str = str + '&' + pair[0] + '=' + pair[1];
+    if (pair[1].indexOf('%') > -1) {
+      // skip
+      str = str + '&' + pair[0] + '=' + pair[1]
     } else {
-      str = str + '&' + pair[0] + '=' + encodeURIComponent(unescape(pair[1]));
+      str = str + '&' + pair[0] + '=' + encodeURIComponent(unescape(pair[1]))
     }
-  }); 
-  return str;
-};
+  })
+  return str
+}
 
 ppIpn.verify = function verify (params, settings, callback) {
-  //Settings are optional, use default settings if not set
+  // Settings are optional, use default settings if not set
   if (typeof callback === 'undefined' && typeof settings === 'function') {
-    callback = settings;
+    callback = settings
     settings = {
       'allow_sandbox': false
-    };
+    }
   }
 
   if (typeof params === 'undefined') {
     process.nextTick(function () {
-      callback(new Error('No params were passed to ipn.verify'));
-    });
-    return;
+      callback(new Error('No params were passed to ipn.verify'))
+    })
+    return
   }
 
-  params.cmd = '_notify-validate';
-  var body = ppIpn.stringify(params);
+  params.cmd = '_notify-validate'
+  var body = ppIpn.stringify(params)
 
-  //Set up the request to paypal
-  var req_options = {
+  // Set up the request to paypal
+  var reqOptions = {
     host: (params.test_ipn) ? ppIpn.SANDBOX_URL : ppIpn.REGULAR_URL,
     method: 'POST',
     path: '/cgi-bin/webscr',
-    headers: {'Content-Length': body.length}
-  };
+    headers: { 'Content-Length': body.length }
+  }
 
   if (params.test_ipn && !settings.allow_sandbox) {
     process.nextTick(function () {
-      callback(new Error('Received request with test_ipn parameter while sandbox is disabled'));
-    });
-    return;
+      callback(new Error('Received request with test_ipn parameter while sandbox is disabled'))
+    })
+    return
   }
 
-  var req = https.request(req_options, function paypal_request(res) {
-    var data = [];
+  var req = https.request(reqOptions, function paypalRequest (res) {
+    var data = []
 
-    res.on('data', function paypal_response(d) {
-      data.push(d);
-    });
+    res.on('data', function paypalResponse (d) {
+      data.push(d)
+    })
 
-    res.on('end', function response_end() {
-      var response = data.join('');
+    res.on('end', function responseEnd () {
+      var response = data.join('')
 
-      //Check if IPN is valid
+      // Check if IPN is valid
       if (response === 'VERIFIED') {
-        callback(null, response);
+        callback(null, response)
       } else {
-        callback(new Error('IPN Verification status: ' + response));
+        callback(new Error('IPN Verification status: ' + response))
       }
-    });
-  });
+    })
+  })
 
-  //Add the post parameters to the request body
-  req.write(body);
+  // Add the post parameters to the request body
+  req.write(body)
 
-  //Request error
-  req.on('error', callback);
+  // Request error
+  req.on('error', callback)
 
-  req.end();
-};
+  req.end()
+}
 
-module.exports = ppIpn;
+module.exports = ppIpn
--- test/ipn.js
+++ test/ipn.js
@@ -1,58 +1,55 @@
-var tap = require('tap');
-var qs = require('querystring');
-var ipn = require('../ipn.js');
-var fixtures = require('./fixtures.js');
-
+var tap = require('tap')
+var qs = require('querystring')
+var ipn = require('../ipn.js')
+var fixtures = require('./fixtures.js')
 
 // Test helper functions
-tap.equal(ipn.stringify({one: '1', two: '2', three: '3'}), '&one=1&two=2&three=3');
-tap.equal(ipn.stringify({one: '%1', two: '2', three: '3'}), '&one=%1&two=2&three=3');
-tap.same(ipn.parseQuery('?mc_gross=19.95'), { 'mc_gross': 19.95 });
-tap.notSame(ipn.parseQuery('?mc_gross=19.00'), { 'mc_gross': 19.95 });
+tap.equal(ipn.stringify({ one: '1', two: '2', three: '3' }), '&one=1&two=2&three=3')
+tap.equal(ipn.stringify({ one: '%1', two: '2', three: '3' }), '&one=%1&two=2&three=3')
+tap.same(ipn.parseQuery('?mc_gross=19.95'), { 'mc_gross': 19.95 })
+tap.notSame(ipn.parseQuery('?mc_gross=19.00'), { 'mc_gross': 19.95 })
 
 tap.test('fake ipn request', function (childTest) {
-  var params = qs.parse(fixtures.invalidIpn());
+  var params = qs.parse(fixtures.invalidIpn())
   ipn.verify(params, { 'allow_sandbox': true }, function callback (err) {
-    childTest.match(err, /Error/);
-    childTest.end();
-  });
-});
+    childTest.match(err, /Error/)
+    childTest.end()
+  })
+})
 
 tap.test('valid ipn request', function (childTest) {
-  var params = qs.parse(fixtures.validIpn());
+  var params = qs.parse(fixtures.validIpn())
   ipn.verify(params, { 'allow_sandbox': true }, function callback (err) {
-    childTest.match(err, null);
-    childTest.end();
-  });
-});
+    childTest.match(err, null)
+    childTest.end()
+  })
+})
 
 tap.test('valid test ipn request but sandbox not allowed', function (childTest) {
-  var params = qs.parse(fixtures.validIpn());
+  var params = qs.parse(fixtures.validIpn())
   ipn.verify(params, { 'allow_sandbox': false }, function callback (err) {
-    childTest.match(err, /Received request with test_ipn parameter while sandbox is disabled/);
-    childTest.end();
-  });
-});
+    childTest.match(err, /Received request with test_ipn parameter while sandbox is disabled/)
+    childTest.end()
+  })
+})
 
 tap.test('valid ipn request with diacritics in item_name', function (childTest) {
   ipn.verify(fixtures.validIpnObject01(), { 'allow_sandbox': true }, function callback (err) {
-    childTest.match(err, null);
-    childTest.end();
-  });
-});
+    childTest.match(err, null)
+    childTest.end()
+  })
+})
 
 tap.test('test without params', function (childTest) {
   ipn.verify(undefined, { 'allow_sandbox': true }, function callback (err) {
-    childTest.match(err, /No params were passed to ipn.verify/);
-    childTest.end();
-  });
-});
+    childTest.match(err, /No params were passed to ipn.verify/)
+    childTest.end()
+  })
+})
 
 tap.test('test without options', function (childTest) {
   ipn.verify(fixtures.validIpnObject01(), function callback (err) {
-    childTest.match(err, /No params were passed to ipn.verify/);
-    childTest.end();
-  });
-});
-
-
+    childTest.match(err, /Received request with test_ipn parameter while sandbox is disabled/)
+    childTest.end()
+  })
+})
